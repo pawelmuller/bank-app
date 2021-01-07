5b5df10e9cf2ff3094dbf68feb1b04c590386e10
Paweł <pawelmuller42@gmail.com>
2021-01-07 13:07:46 +0100

    Added custom fonts.

--- src/main/java/com/bllk/Apka/CustomPanels.java
+++ src/main/java/com/bllk/Apka/CustomPanels.java
@@ -1,6 +1,10 @@
 package com.bllk.Apka;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 class AccountPanel extends JPanel {
     public AccountPanel(String _account_number, String _balance, String _currency) {
@@ -16,5 +20,24 @@ class AccountPanel extends JPanel {
         this.add(accountNumber);
         this.add(balanceLabel);
         this.add(currencyLabel);
+    }
+}
+
+class Fonts {
+    public Font radikal;
+    public Font adagio_slab;
+
+    public Fonts() {
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            radikal = Font.createFont(Font.TRUETYPE_FONT, new File("src/main/fonts/radikalwut-bold.otf"));
+            adagio_slab = Font.createFont(Font.TRUETYPE_FONT, new File("src/main/fonts/adagio_slab-regular.otf"));
+            ge.registerFont(radikal);
+            ge.registerFont(adagio_slab);
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
