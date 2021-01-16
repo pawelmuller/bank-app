1531ab0a9ffad9aa0611a111b3b8a86aa2677e5f
Paweł <pawelmuller42@gmail.com>
2021-01-17 00:52:03 +0100

    CamelCased Colors.java.

--- src/main/java/com/bllk/Apka/resourceHandlers/Colors.java
+++ src/main/java/com/bllk/Apka/resourceHandlers/Colors.java
@@ -3,20 +3,20 @@ package com.bllk.Apka.resourceHandlers;
 import java.awt.*;
 
 public class Colors {
-    private static Color bright_text_color, orange, blue, dark_grey, bright_grey, grey, light_grey;
+    private static Color brightTextColor, orange, blue, darkGrey, grey, lightGrey, brightGrey;
 
     public Colors() {
-        bright_text_color = Color.decode("#EEEEEE");
+        brightTextColor = Color.decode("#EEEEEE");
         blue = Color.decode("#1891FF");
         orange = Color.decode("#FF7F00");
-        dark_grey = Color.decode("#222222");
+        darkGrey = Color.decode("#222222");
         grey = Color.decode("#333333");
-        light_grey = Color.decode("#444444");
-        bright_grey = Color.decode("#808080");
+        lightGrey = Color.decode("#444444");
+        brightGrey = Color.decode("#808080");
     }
 
     public static Color getBrightTextColor() {
-        return bright_text_color;
+        return brightTextColor;
     }
     public static Color getOrange() {
         return orange;
@@ -25,15 +25,15 @@ public class Colors {
         return blue;
     }
     public static Color getDarkGrey() {
-        return dark_grey;
+        return darkGrey;
     }
     public static Color getGrey() {
         return grey;
     }
     public static Color getLightGrey() {
-        return light_grey;
+        return lightGrey;
     }
     public static Color getBrightGrey() {
-        return bright_grey;
+        return brightGrey;
     }
 }
